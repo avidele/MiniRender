861a2a8b8277a2d2d40b7bcfa675300d0b48d094
Avidel_Arch <1215935448@qq.com>
2025-03-24 00:25:51 +0800

    03_triangle Init

---
+++ examples/03_triangle/main.cpp
@@ -0,0 +1,14 @@
+/*
+ * @Author: Avidel_Arch 1215935448@qq.com
+ * @Date: 2025-03-23 23:58:13
+ * @LastEditors: Avidel_Arch 1215935448@qq.com
+ * @LastEditTime: 2025-03-24 00:25:28
+ * @FilePath: /MiniRender/examples/03_triangle/main.cpp
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AEN
+ */
+#include "SDL3/SDL_init.h"
+#define SDL_MAIN_USE_CALLBACKS
+
+SDL_AppResult SDL_AppInit(void** appstate, int argc, char* argv[]){
+    return SDL_APP_CONTINUE;
+}
