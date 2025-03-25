e35841e76fb1108f0b5c53088e45fca457322ba1
Avidel_Arch <1215935448@qq.com>
2025-03-26 01:04:56 +0800

    03-2 InitVulkan

--- examples/03_triangle/main.cpp
+++ examples/03_triangle/main.cpp
@@ -2,10 +2,11 @@
  * @Author: Avidel_Arch 1215935448@qq.com
  * @Date: 2025-03-23 23:58:13
  * @LastEditors: nolanyzhang
- * @LastEditTime: 2025-03-25 00:26:58
+ * @LastEditTime: 2025-03-26 01:12:47
  * @FilePath: /MiniRender/examples/03_triangle/main.cpp
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AEN
  */
+#include <vector>
 #define SDL_MAIN_USE_CALLBACKS
 #include "SDL3/SDL_init.h"
 #include "SDL3/SDL_vulkan.h"
@@ -13,6 +14,7 @@
 #include <vulkan/vulkan_core.h>
 #include <SDL3/SDL_main.h>
 
+#define EnableDebug 1
 struct Vertex {
     float pos[2];
     float color[3];
@@ -49,6 +51,7 @@ private:
 };
 
 void InitVulkan(){
+    auto sdlVkInstance = SDLVkInstance::GetInstance();
     auto window = SDLVkInstance::GetInstance()->window;
     VkApplicationInfo appInfo{
         .sType = VK_STRUCTURE_TYPE_APPLICATION_INFO,
@@ -59,11 +62,43 @@ void InitVulkan(){
 
     uint32_t extensionCount = 0;
     SDL_Vulkan_GetInstanceExtensions(&extensionCount);
+#if EnableDebug
     std::cout << "Vulkan extension count: " << extensionCount << std::endl;
     for(uint32_t i = 0; i < extensionCount; ++i){
         const char* extensionName = SDL_Vulkan_GetInstanceExtensions(&extensionCount)[i];
         std::cout << "Vulkan extension: " << extensionName << std::endl;
     }
+#endif
+
+    VkInstanceCreateInfo createInfo{
+        .sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO,
+        .pApplicationInfo = &appInfo,
+        .enabledExtensionCount = extensionCount,
+        .ppEnabledExtensionNames = SDL_Vulkan_GetInstanceExtensions(&extensionCount),
+    };
+
+    if(vkCreateInstance(&createInfo, nullptr, &sdlVkInstance->instance) != VK_SUCCESS){
+        std::cerr << "Failed to create Vulkan instance!" << std::endl;
+        exit(EXIT_FAILURE);
+    }
+
+    if(!SDL_Vulkan_CreateSurface(window, sdlVkInstance->instance, nullptr, &sdlVkInstance->surface)){
+        std::cerr << "Failed to create Vulkan surface!" << std::endl;
+        exit(EXIT_FAILURE);
+    }
+
+    uint32_t deviceCount = 0;
+    vkEnumeratePhysicalDevices(sdlVkInstance->instance, &deviceCount, nullptr);
+    std::vector<VkPhysicalDevice> devices(deviceCount);
+    vkEnumeratePhysicalDevices(sdlVkInstance->instance, &deviceCount, devices.data());
+#if EnableDebug
+    std::cout << "Vulkan device count: " << deviceCount << std::endl;
+    for(uint32_t i = 0; i < deviceCount; ++i){
+        VkPhysicalDeviceProperties deviceProperties;
+        vkGetPhysicalDeviceProperties(devices[i], &deviceProperties);
+        std::cout << "Vulkan device: " << deviceProperties.deviceName << std::endl;
+    }
+#endif
 }
 
 SDL_AppResult SDL_AppInit(void** appstate, int argc, char* argv[]){
@@ -85,5 +120,9 @@ SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event){
 }
 
 SDL_AppResult SDL_AppIterate(void *appstate){
+    SDL_Init(SDL_INIT_VIDEO);
+    SDLVkInstance::GetInstance()->window = SDL_CreateWindow("Dynamic Triangle",  800, 600, SDL_WINDOW_VULKAN);
+    InitVulkan();
     return SDL_APP_CONTINUE;
 }
+
